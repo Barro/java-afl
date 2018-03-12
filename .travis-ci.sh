13fe13dbf654abc21eafb84093d75ac38960149c
Jussi Judin <jjudin+java-afl@iki.fi>
2018-03-12 09:40:25 +0200

    Add Travis CI support

---
+++ .travis-ci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+INSTALL_DIR=$DIR/installs
+
+wget http://lcamtuf.coredump.cx/afl/releases/afl-2.52b.tgz
+tar xf afl-2.52b.tgz
+cd afl-2.52b
+make -j "$(nproc)" PREFIX="$INSTALL_DIR"
+make install PREFIX="$INSTALL_DIR"
+
+export PATH=$INSTALL_DIR/bin:$PATH
+cd "$DIR"
+
+./build.sh
